3226dbbbc7d545ad6fdcce944a5a3bb96e926a8a
Xyuan <906162099@qq.com>
2025-02-10 17:07:51 +0800

    feat:表单 checkbox 组件添加全选功能

--- src/components/ins-form/components/checkbox/index.vue
+++ src/components/ins-form/components/checkbox/index.vue
@@ -18,6 +18,10 @@ const props = defineProps({
     type: Array,
     default: () => [],
   },
+  hasCheckAll: {
+    type: Boolean,
+    default: false,
+  },
 })
 
 useFetchData(props.formItem)
@@ -25,7 +29,23 @@ useFetchData(props.formItem)
 const emit = defineEmits(['update:modelValue', 'change'])
 
 /* 值的双向绑定 */
-const modelValue = useVModel(props, 'modelValue', emit) // 值的双向绑定
+const checkboxValue = useVModel(props, 'modelValue', emit) // 值的双向绑定
+
+const checkAll = computed(() => {
+  if (props.formItem.options.length === 0) return false
+  return checkboxValue.value.length === props.formItem.options.length
+})
+
+const isIndeterminate = computed(() => {
+  return (
+    checkboxValue.value.length > 0 && checkboxValue.value.length < props.formItem.options.length
+  )
+})
+
+function handleCheckAllChange(val) {
+  checkboxValue.value = val ? props.formItem.options.map((item) => item.value) : []
+  emit('change')
+}
 
 function handleChange() {
   emit('change')
@@ -33,8 +53,16 @@ function handleChange() {
 </script>
 
 <template>
+  <el-checkbox
+    v-if="hasCheckAll"
+    :model-value="checkAll"
+    :indeterminate="isIndeterminate"
+    @change="handleCheckAllChange"
+  >
+    全选
+  </el-checkbox>
   <el-checkbox-group
-    v-model="modelValue"
+    v-model="checkboxValue"
     class="ins-checkbox"
     v-bind="$attrs"
     @change="handleChange"
